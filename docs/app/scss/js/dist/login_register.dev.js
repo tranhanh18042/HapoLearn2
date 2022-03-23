91cf64948d885069875631d254ba7b02192a161b
tranhanh18042 <hanhtran18042@gmail.com>
2022-03-23 11:02:37 +0700

    add form login register , messenger

---
+++ docs/app/scss/js/dist/login_register.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".btn-register").click(function () {
+    $(".content-login").css('display', 'none');
+    $(".content-register").css('display', 'block');
+  });
+  $(".btn-login-header").click(function () {
+    $(".content-register").css('display', 'none');
+    $(".content-login").css('display', 'block');
+  });
+});
